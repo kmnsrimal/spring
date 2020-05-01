03c00d5d799c8dc5dcc8548783f0a595375dbc36
kmnsrimal <nsrimal718@gmail.com>
2020-05-02 00:11:22 +0530

    first springboot application

---
+++ spring-boot-crud-example-2/src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -0,0 +1,29 @@
+package com.javatechie.crud.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+    
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private int quentity;
+	private double price;
+	
+	
+	
+	
+}
